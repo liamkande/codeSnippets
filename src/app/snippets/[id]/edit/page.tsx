847f47134da9257e33de49db2ac646c3c3800a14
Liam Ansoumane Kande <liamkande@icloud.com>
2025-04-04 11:19:15 -0700

    adds and links SnippetEditForm

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,11 +1,28 @@
+import { notFound } from 'next/navigation'
+import { db } from '@/db'
+import SnippetEditForm from '@/components/snippet-edit-form'
+
 interface SnippetEditPageProps {
-  params: {
+  params: Promise<{
     id: string
-  }
+  }>
 }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
-  const id = parseInt(props.params.id)
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const { id } = await props.params
 
-  return <div>Editing snippet with id {id}</div>
+  const snippetId = parseInt(id)
+  const snippet = await db.snippet.findFirst({
+    where: { id: snippetId },
+  })
+
+  if (!snippet) {
+    return notFound()
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  )
 }
