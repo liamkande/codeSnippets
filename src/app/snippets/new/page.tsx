d6394b57e949c0ec8a1f34f37a097820da404470
Liam Ansoumane Kande <liamkande@icloud.com>
2025-04-22 20:01:33 -0700

    refactored and adds useActionState to track formState validation.

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,29 +1,16 @@
-import { redirect } from 'next/navigation'
-import { db } from '@/db'
+'use client'
+
+import { useActionState } from 'react'
+
+import * as actions from '@/actions'
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // This needs to be a server action!
-    'use server'
-
-    // Check the user's inputs and make sure they're valid
-    const title = formData.get('title') as string
-    const code = formData.get('code') as string
-
-    // Create a new record in the database
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    })
-
-    // Redirect the user back to the root route
-    redirect('/')
-  }
+  const [formState, action] = useActionState(actions.createSnippet, {
+    message: '',
+  })
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -47,6 +34,8 @@ export default function SnippetCreatePage() {
             id="code"
           />
         </div>
+
+        <div>{formState.message}</div>
 
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
