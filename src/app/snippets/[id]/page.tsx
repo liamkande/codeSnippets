7cd282445e88adea22cb993513f85f99c7aa9232
Liam Ansoumane Kande <liamkande@icloud.com>
2025-04-22 21:32:35 -0700

    adds cache controls for dynamic routes in production mode.

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -46,3 +46,14 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     </div>
   )
 }
+
+// Remember it has to be called generateStaticParams
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany()
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    }
+  })
+}
