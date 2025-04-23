dbe82424b6284026355b078c660ec75af7f65e64
Liam Ansoumane Kande <liamkande@icloud.com>
2025-04-22 20:36:21 -0700

    adds server side error handling.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -24,31 +24,45 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // Check the user's inputs and make sure they're valid
-  const title = formData.get('title')
-  const code = formData.get('code')
+  try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get('title')
+    const code = formData.get('code')
 
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: 'Title must be longer',
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer',
+      }
+    }
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be longer',
+      }
+    }
+
+    // Create a new record in the database
+
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    })
+    // test only
+    // throw new Error('Failed to save to database.')
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      }
+    } else {
+      return {
+        message: 'Something went wrong...',
+      }
     }
   }
-  if (typeof code !== 'string' || code.length < 10) {
-    return {
-      message: 'Code must be longer',
-    }
-  }
-
-  // Create a new record in the database
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  })
-  // will need to be handled in case we get an error when submitting data to the database.
-  throw new Error('Not implemented yet')
 
   // Redirect the user back to the root route
+  //remember we do not want to put our redirect in the try/catch block
   redirect('/')
 }
