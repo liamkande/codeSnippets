f19eeff27bf08b28e0e846e7ba861a2f767b7d4c
Liam Ansoumane Kande <liamkande@icloud.com>
2025-04-22 19:31:43 -0700

    updates the prisma database

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,13 @@
 'use server'
 
 import { redirect } from 'next/navigation'
+import { db } from '@/db'
 
 export async function editSnippet(id: number, code: string) {
-  console.log(`Editing snippet with ID: ${id}, with: ${code}`)
-
+  // console.log(`Editing snippet with ID: ${id}, with: ${code}`)
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  })
   redirect(`/snippets/${id}`)
 }
